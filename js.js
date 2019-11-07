3a058a56fb637e63c197056a27d3e2e9cfef4648
Aryeh (Ari) Tsetlin <57076256+ATsetlin@users.noreply.github.com>
2019-11-07 22:15:05 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,19 +1,42 @@
 var word = "bottles";
-var word2 = "bottle"
-var count = 99;
+var word2 = "bottle";
+var count = prompt("How many bottle of beer?");
+if (count == 1) {
+	console.log(count + " " + word2 + " of beer on the wall,");
+	console.log(count + " " + word2 + " of beer,");
+	console.log("take it down, pass it around,");
+	console.log("Oh CRAP!")
+	console.log("WE NEED MORE BEEEEEEERRRRRRRR!!!!!!!!")
+}
 
-while (count > 0) {
-	console.log(count + " " + word + " of beer on the wall");
-	console.log(count + " " + word + " of beer");
-	console.log("Take one down, pass it around,");
-	count = count - 1;
-	if (count > 1) {
+if (count > 1)
+for (drank = 1; drank < count; drank++) {
+	console.log(count + " " + word + " of beer on the wall,");
+	console.log(count + " " + word + " of beer,");
+
+	if (drank == 1 || count == 1){
+	console.log("Take " + drank + " down, pass it around,");
+	count = count - drank;
+	console.log(count + " " + word + " of beer on the wall.");
+	}
+	else if (drank > 1 ) {
+		console.log("Take " + drank + " down, pass them around,");
+		count = count - drank;
 		console.log(count + " " + word + " of beer on the wall.");
 	}
-	else if (count == 1) {
-			console.log(count + " " + word2 + " of beer on the wall.");
-	}
-	else {
-			console.log("no more " + word + " of beer on the wall.");
-	}
 }
+
+if (count == drank || count >=  2) {
+		console.log(count + " " + word + " of beer on the wall,");
+		console.log(count + " " + " bottle of beer,");
+		console.log("Take " + (count -1) + " down, drinks all around,");
+		count = 1;
+		console.log(count + " " +  word2 + " of beer on the wall.");
+}
+
+var count = 1 
+	console.log(count + " " + word2 + " of beer on the wall,");
+	console.log(count + " " + word2 + " of beer,");
+	console.log("take it down, pass it around,");
+	console.log("Oh CRAP!")
+	console.log("WE NEED MORE BEEEEEEERRRRRRRR!!!!!!!!")
